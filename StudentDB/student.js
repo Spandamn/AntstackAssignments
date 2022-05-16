b97e31bb4dd0ceb80eab320b6c4950d3b0a3c883
Spandan Punwatkar <spandan.punwatkar@gmail.com>
2022-05-16 13:31:35 +0530

    Update student.js

--- StudentDB/student.js
+++ StudentDB/student.js
@@ -2,23 +2,41 @@ const express = require('express');
 const app = express();
 const studentdb = {
 };
-let idNums = 0;
+const awsConfig = require('../awsConfig.json');
+let aws = require('aws-sdk');
+aws.config.update(awsConfig);
+let docClient = new aws.DynamoDB.DocumentClient();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.get("/", (req, res) => {
     res.render('student');
 });
+let idNums = 0;
 
 app.get("/api/student", async (req, res) => {
-    res.json(studentdb);
+    docClient.scan({"TableName": "StudentDB"}, function (err, data) {
+        if (err) {
+            res.send("ERROR: " + JSON.stringify(err));
+        } else {
+            res.json(data); // Scan
+        }
+    });
 });
 app.get("/api/student/:studentid", async (req, res) => {
     let studentid = req.params.studentid;
-    if (studentid in studentdb) {
-        res.json(studentdb[studentid]);
-    } else {
-        res.sendStatus(400);
-    }
+    let studentObj;
+    console.log(studentid);
+    docClient.get({TableName: "StudentDB", Key: {"id": parseInt(studentid)}}, function (err, data) { // parseInt because in db, the id is a number
+        if (err) {
+            res.send("ERROR: " + JSON.stringify(err));
+        } else {
+            if (data.Item) {
+                res.json(data.Item);
+            } else {
+                res.sendStatus(400); // Read an Item
+            }
+        }
+    });
 });
 app.post("/api/student", (req, res) => {
     let id = ++idNums;
@@ -26,28 +44,68 @@ app.post("/api/student", (req, res) => {
     if (!('name' in data || 'currentClass' in data || 'Division' in data)) {
         res.sendStatus(400);
     } else {
-        studentdb[`${id}`] = {...data, id};
-        res.send(studentdb[`${id}`]);
+        let student = {...data, id};
+        docClient.put({"TableName": "StudentDB", Item: student}, function (err, data) {
+            if (err) {
+                res.send("ERROR: " + JSON.stringify(err));
+            } else {
+                res.send(student); // Write
+            }
+        });
     }
 });
 app.put("/api/student/:studentid", (req, res) => {
     let id = req.params.studentid;
     let data = req.body;
-    if (!('name' in data || 'currentClass' in data || 'Division' in data || id in studentdb)) {
+    if (!('name' in data || 'currentClass' in data || 'Division' in data)) {
         res.sendStatus(400);
     } else {
-        studentdb[`${id}`] = {...data, id};
-        res.send(studentdb[`${id}`]);
+        let updateEx = `set ${'name' in data ? '#n = :nn': ''}${'name' in data && Object.keys(data).length > 1 ? ', ' : ''}`;
+        updateEx += `${'currentClass' in data ? 'currentClass = :cc': ''}`;
+        updateEx += `${'currentClass' in data && Object.keys(data).length > ('name' in data ? 2 : 1) ? ', ' : ''}`;
+        updateEx += `${'Division' in data ? 'Division = :d': ''}`;
+        console.log(updateEx);
+        let exAtNames = {};
+        let exAtValues = {};
+        if ('name' in data) {
+            exAtNames['#n'] = 'name';
+            exAtValues[':nn'] = data.name;
+        }
+        if ('currentClass' in data) {
+            exAtValues[':cc'] = data.currentClass;
+        }
+        if ('Division' in data) {
+            exAtValues[':d'] = data.Division;
+        }
+        console.log(exAtValues);
+        let params = {ConditionExpression: "attribute_exists(id)", ExpressionAttributeNames:exAtNames, UpdateExpression: updateEx, ExpressionAttributeValues:exAtValues, TableName: "StudentDB", Key: {"id": parseInt(id)}};
+        console.log(params);
+        docClient.update(params, function (err, data) {
+            if (err) {
+                if (err.code === "ConditionalCheckFailedException") {
+                    res.sendStatus(404);
+                } else {
+                    res.send("ERROR: " + JSON.stringify(err));
+                }
+            } else {
+                res.send(data); // Write
+            }
+        });
     }
 });
 app.delete("/api/student/:studentid", (req, res) => {
-    let id = req.params.studentid;
-    if (!((`${id}`) in studentdb)) {
-        res.sendStatus(404);
-    } else {
-        delete studentdb[`${id}`];
-        res.send(`Deleted ${id}`);
-    }
+    let id = req.params.studentid; // Delete
+    docClient.delete({ConditionExpression: "attribute_exists(id)", TableName: "StudentDB", Key: {"id": parseInt(id)}}, function(err, data) {
+        if (err) {
+            if (err.code === "ConditionalCheckFailedException") {
+                res.sendStatus(404);
+            } else {
+                res.send("ERROR: " + JSON.stringify(err));
+            }
+        } else {
+            res.send("SUCCESS: " + JSON.stringify(data));
+        }
+    });
 });
 
 app.listen(PORT, () => console.log(`server started, listening PORT ${PORT}`));
